fe720b1d1d4d9259586c86dddb8302d591b821fa
Scruickshank13 <78326348+Scruickshank13@users.noreply.github.com>
2024-06-26 15:40:39 +0000

    Hello World Program Test

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Learning C++ 
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Scott C
+
+#include<iostream>
+
+using namespace std;
+
+int main () {
+cout << "Hello World" << endl;
+
+}
