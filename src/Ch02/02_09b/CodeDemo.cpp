d2061053c8c75b72db81a937144d2735d09c9536
Scruickshank13 <78326348+Scruickshank13@users.noreply.github.com>
2024-06-27 20:00:19 +0000

    Array concat testing

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -1,11 +1,25 @@
 // Learning C++ 
 // Exercise 02_09
-// Strings, by Eduardo Corpeño 
+// Strings, by Scott 
 
 #include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
 
 int main(){
-    
+   const size_t LENGTH1 = 25;
+
+   char array_str1[LENGTH1] = "Hey guys! ";
+   char array_str2[] = "What's up?";
+
+   string std_str1 = "Hi Hi ";
+   string std_str2 = "How are you?";
+
+   strncat(array_str1, array_str2, LENGTH1);
+   cout << array_str1 << endl;
+   cout << std_str1 + std_str2 << endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
