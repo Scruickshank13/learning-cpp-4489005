58846ec18b6121db824ad86e596aed27c27c9075
Scruickshank13 <78326348+Scruickshank13@users.noreply.github.com>
2024-06-27 18:23:57 +0000

    Array practice

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -1,10 +1,32 @@
 // Learning C++ 
 // Exercise 02_08
-// Arrays, by Eduardo Corpeño 
+// Arrays, by Scott 
 
 #include <iostream>
+using namespace std;
+
+// #define AGE_LENGTH 4
 
 int main(){
+    const size_t AGE_LENGTH = 4; 
+    int age [AGE_LENGTH];
+    float temperature[] = {31.5, 32.7, 38.9};
+    
+    age[0] = 25;
+    age[1] = 20;
+    age[2] = 19;
+    age[3] = 19;
+    
+    cout << "Age [0] = " << age[0] << endl;
+    cout << "Age [1] = " << age[1] << endl;
+    cout << "Age [2] = " << age[2] << endl;
+    cout << "Age [3] = " << age[3] << endl;
+
+    cout << endl;
+
+    cout << "Temperature [0] = " << temperature[0] << endl;
+    cout << "Temperature [1] = " << temperature[1] << endl;
+    cout << "Temperature [2] = " << temperature[2] << endl;
     
     std::cout << std::endl << std::endl;
     return (0);
