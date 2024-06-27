650c338847da00a323f13497b1cb4bd40b7c0f91
Scruickshank13 <78326348+Scruickshank13@users.noreply.github.com>
2024-06-27 22:51:03 +0000

    Array average implementation

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -1,15 +1,17 @@
 // Learning C++ 
 // Challenge 02_13
-// Calculate an Average, by Eduardo Corpeño 
+// Calculate an Average, by Scott 
 
 #include <iostream>
+using namespace std;
 
 int main(){
-    int nums[5] = {1,23,32,24,337};
-    float result; 
+   float nums[5] = {1, 34, 54.5, 45, 6};
+   float result;
 
-    // Write your code here
-    
+    result = nums[0] + nums[1] + nums[2] + nums[3] + nums[4];
+    result /=5;
+
     std::cout << "The average is " << result << std::endl;
     
     std::cout << std::endl << std::endl;
