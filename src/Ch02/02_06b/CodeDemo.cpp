8b615fd826d9bb3961a540d3c5368d22730d296b
Scruickshank13 <78326348+Scruickshank13@users.noreply.github.com>
2024-06-27 16:13:16 +0000

    Directive PRactice

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -1,11 +1,22 @@
 // Learning C++ 
 // Exercise 02_06
-// Preprocessor directives, by Eduardo Corpeño 
+// Preprocessor directives, by Scott 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+using namespace std;
+
+#define CAPACITY 5000
 
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG
+    cout << "[About to perform the addtion]" << endl;
+#endif 
+    large += small;
+
+    cout << "The large integer is " << large << endl;
     return (0);
 }
