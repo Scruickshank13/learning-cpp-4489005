7b2a092f21dcf87f51277ad5ea5c2a0a415ed93d
Scruickshank13 <78326348+Scruickshank13@users.noreply.github.com>
2024-06-28 16:55:02 +0000

    Pointer practice

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -1,11 +1,22 @@
 // Learning C++ 
 // Exercise 03_06
-// Pointers, by Eduardo Corpeño 
+// Pointers, by Scott 
 
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main(){
+    int a = 37;
+    int *ptr;
+
+    ptr = &a;
+
+    cout << "The content of a is " << a << endl;
+    cout << "The pointer is pointing to address " << ptr << endl;
+    cout << "The address of a is " << &a << endl;
+    cout << "Where the pointer is pointing, we have " << *ptr << endl;
+    cout << "The address of ptr is " << &ptr << endl;
 
     std::cout << std::endl << std::endl;
     return (0);
